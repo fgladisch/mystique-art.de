3a5e8ea1e9aa27a77bcc13f4f2bc8f867393cf39
Felix Gladisch <gladisch@sipgate.de>
2018-02-12 17:21:55 +0100

    design: font size

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Headline = (props) => (
-  <h1 className="display-5 text-center text-uppercase">{props.children}</h1>
+  <h2 className="text-center text-uppercase">{props.children}</h2>
 )
 
 export default Headline;
