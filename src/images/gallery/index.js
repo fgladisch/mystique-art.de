600080f775f94585d1193358f22532b86adbcb7b
Felix Gladisch <felix@felixgladisch.de>
2018-11-22 00:39:50 +0100

    update

--- src/images/gallery/index.js
+++ src/images/gallery/index.js
@@ -1,5 +1,14 @@
 import alienCira1 from "./alien_cira_1.jpg";
 import alienCira2 from "./alien_cira_2.jpg";
+import anika1 from "./anika_1.jpg";
+// import anika2 from "./anika_2.jpg";
+// import anika3 from "./anika_3.jpg";
+// import anika4 from "./anika_4.jpg";
+// import anika5 from "./anika_5.jpg";
+import anika6 from "./anika_6.jpg";
+import clara1 from "./clara_1.jpg";
+import clara2 from "./clara_2.jpg";
+import clara3 from "./clara_3.jpg";
 import cira1 from "./cira_1.jpg";
 import cira2 from "./cira_2.jpg";
 import cira3 from "./cira_3.jpg";
@@ -13,15 +22,39 @@ import lisa2 from "./lisa_2.jpg";
 import lisa3 from "./lisa_3.jpg";
 import merle1 from "./merle_1.jpg";
 import merle2 from "./merle_2.jpg";
+// import merle3 from "./merle_3.jpg";
+import merle4 from "./merle_4.jpg";
+import merle5 from "./merle_5.jpg";
+import merle6 from "./merle_6.jpg";
 import sandra1 from "./sandra_1.jpg";
 import sandra2 from "./sandra_2.jpg";
 import sandra3 from "./sandra_3.jpg";
+import sandra4 from "./sandra_4.jpg";
+import sandra5 from "./sandra_5.jpg";
+import sandra6 from "./sandra_6.jpg";
+// import sandra7 from "./sandra_7.jpg";
+// import sandra8 from "./sandra_8.jpg";
+// import sandra9 from "./sandra_9.jpg";
+// import sandra10 from "./sandra_10.jpg";
+import sandra11 from "./sandra_11.jpg";
 import stina from "./stina.jpg";
 import tatjana1 from "./tatjana_1.jpg";
 import tatjana2 from "./tatjana_2.jpg";
 import tatjana3 from "./tatjana_3.jpg";
 
 const gallery = [
+	anika1,
+	clara1,
+	merle4,
+	anika6,
+	sandra11,
+	clara2,
+	sandra5,
+	merle5,
+	merle6,
+	sandra4,
+	sandra6,
+	clara3,
 	lisa1,
 	cira3,
 	sandra3,
